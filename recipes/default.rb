00e4309be9ef949415bc14f9940c7af118480bf3
David ML Brown Jr <dmlb2000@uber-gamer.dmlb2000.org>
2016-01-17 22:08:25 -0800

    revert previous commit

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: dmlb2000_server
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+include_recipe 'zfs_linux'
+include_recipe 'dmlb2000_users'
+include_recipe 'samba'
+include_recipe 'samba::server'
+
