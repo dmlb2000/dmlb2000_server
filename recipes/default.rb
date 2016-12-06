e869704166d525fb0bfec5ef0722f132781313fb
David Brown <dmlb2000@gmail.com>
2016-12-05 19:12:57 -0800

    try adding some zfs fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'zfs_linux'
+r = resources(package: 'zfs-release')
+r.source('http://download.zfsonlinux.org/epel/zfs-release.el7.noarch.rpm')
 include_recipe 'dmlb2000_users'
 include_recipe 'dmlb2000_docker'
 include_recipe 'samba'
